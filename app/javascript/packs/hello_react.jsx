64023eae17868eae8665facf653cc51cb00e58ab
Bran Liang <lby89757@hotmail.com>
2017-10-15 23:29:14 +0800

    JS style fixed

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    <Hello name='React' />,
+    document.body.appendChild(document.createElement('div'))
   )
 })
