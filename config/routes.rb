dc86449072c2af5c135f658ebdefff4e38515d00
Bran Liang <lby89757@hotmail.com>
2017-10-16 23:31:25 +0800

    Config the sidekiq with webui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'hello_world', to: 'hello_world#index'
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
