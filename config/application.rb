98b0abe62def28bb805df1afed1de4b4804f7e7e
Bran Liang <lby89757@hotmail.com>
2017-10-16 22:44:21 +0800

    Config the sidekiq

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RailsBoilerplate
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.active_job.queue_adapter = :sidekiq
+
     config.middleware.use Rack::Attack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
